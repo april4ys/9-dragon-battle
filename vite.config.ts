5ff4cb89b28361f226383630635ab8ece57b986e
soso <soso@soso.local>
2026-05-30 22:23:07 +0900

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/9-dragon-battle/" : "/",
   plugins: mode === "https" ? [react(), basicSsl()] : [react()],
   server: {
     host: "0.0.0.0",
